6438f49c3d8110e71c4f1492e4caa3af8a3fb791
Tagliassi <rafael.galafassi2005@gmail.com>
2024-10-21 14:24:37 -0300

    Day 02.1

---
+++ revision.py
@@ -0,0 +1,33 @@
+List = []
+
+List.append("Banana")
+x = List.count("Banana")
+
+print(x)
+
+print(List.index("Banana"))
+
+List.insert(1, "Apple")
+
+print(List)
+
+List.pop(1)
+
+List.remove("Banana")
+
+List.insert(0, "Banana")
+List.append("apple")
+
+List.reverse()
+
+print(List)
+
+texto = "Eu gosto muito de python e python gosta muito de mim"
+
+y = texto.count("python")
+
+z = texto.find("python")
+
+print(z)
+
+print(f"Python aparece {y} vezes na frase")
